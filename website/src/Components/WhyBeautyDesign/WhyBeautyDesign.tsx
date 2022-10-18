1b37df9ba5720941def306cf9dcc95f6601e7d5e
Diego <diego2000avelar@gmail.com>
2022-10-18 09:56:35 -0600

    feat: translation why section

--- website/src/Components/WhyBeautyDesign/WhyBeautyDesign.tsx
+++ website/src/Components/WhyBeautyDesign/WhyBeautyDesign.tsx
@@ -10,12 +10,18 @@ import {
 import Title from '@/Components/Title';
 
 import { WrapperStyle } from './style';
+import { useTranslation } from 'next-i18next';
 
-const WhyBeautyDesign = () => {
+type WhyBeautyDesignProps = {};
+const WhyBeautyDesign = (props: WhyBeautyDesignProps) => {
+  const { t } = useTranslation();
   return (
     <WrapperStyle className="mt-8 mb-8">
       <Title className="text-center">
-        Why{' '}
+        {t(
+          'home:why-section.title',
+          'Why '
+        )}
         <Text
           as="span"
           css={{
@@ -29,29 +35,54 @@ const WhyBeautyDesign = () => {
         <Grid xs={12} sm={6} md={4} lg={3}>
           <CardWhy
             icon={<DesktopIcon />}
-            title="Beautiful Design"
-            content="Create bright, beautiful and full of life apps, we have 25+ reusable components."
+            title={t(
+              'home:why-section.card1.title',
+              'Beautiful Design'
+            )}
+            content={t(
+              'home:why-section.card1.description',
+              'Create bright, beautiful and full of life apps, we have 25+ reusable components.'
+              )}
           />
         </Grid>
         <Grid xs={12} sm={6} md={4} lg={3}>
           <CardWhy
             icon={<CodeIcon />}
-            title="Easy to use"
-            content="Beautiful and vibrant applications with just one import, reduce development times and let us design for you."
+            title={t(
+              'home:why-section.card2.title',
+              'Easy to use'
+              )}
+            content={t(
+              'home:why-section.card2.description',
+              'Beautiful and vibrant applications with just one import, reduce development times and let us design for you.'
+            )}
           />
         </Grid>
         <Grid xs={12} sm={6} md={4} lg={3}>
           <CardWhy
             icon={<PaletteIcon />}
-            title="Customizable"
-            content="Customize each element of the components and change their appearance in dark and light mode."
+            title={t(
+              'home:why-section.card3.title',
+              'Customizable'
+            )}
+
+            content={t(
+              'home:why-section.card3.description',
+              'Customize each element of the components and change their appearance in dark and light mode.'
+            )}
           />
         </Grid>
         <Grid xs={12} sm={6} md={4} lg={3}>
           <CardWhy
             icon={<HappyIcon />}
-            title="Progressive updates"
-            content="We try to keep the library up to date and compatible with the latest versions of React, React Native and Expo."
+            title={t(
+              'home:why-section.card4.title',
+              'Progressive updates'
+            )}
+            content={t(
+              'home:why-section.card4.description',
+              'We try to keep the library up to date and compatible with the latest versions of React, React Native and Expo.'
+            )}
           />
         </Grid>
       </Grid.Container>
