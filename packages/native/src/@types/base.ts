788b542e94c844175c8be4433da3cdebe24e41d6
Kevin <kevin.rivaslive@gmail.com>
2023-12-02 17:10:00 -0600

    fix: fixed style prop

--- packages/native/src/@types/base.ts
+++ packages/native/src/@types/base.ts
@@ -1,5 +1,7 @@
 import { SxProps } from '../lib/styleDictionary';
+import { StyleProp, TextStyle, ViewStyle } from 'react-native';
 
 export type BaseProps = Partial<SxProps> & {
   sx?: SxProps;
+  style?: StyleProp<ViewStyle & TextStyle>;
 };
