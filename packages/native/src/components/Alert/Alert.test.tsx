5d2754718a8a075191ee1632f2ef1f75efb86930
Kevin <kevin.rivaslive@gmail.com>
2024-06-29 22:25:44 -0600

    feat(packages/native): added apearance in buttons and onion alert and message components

--- packages/native/src/components/Alert/Alert.test.tsx
+++ packages/native/src/components/Alert/Alert.test.tsx
@@ -3,7 +3,7 @@ import renderer, { ReactTestRendererJSON } from 'react-test-renderer';
 import { Alert } from './Alert';
 
 describe('<App />', () => {
-  it('has 1 child', () => {
+  it('has 1 child test', () => {
     const component = renderer.create(<Alert content="Alert message" />);
     const tree = component.toJSON() as ReactTestRendererJSON;
     expect(tree.children.length).toBe(1);
