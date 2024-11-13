61f480f0692e002350193953892708f89fec6d7a
Kevin <kevin.rivaslive@gmail.com>
2024-11-13 09:06:32 -0600

    chore: remove lib build

--- packages/reactivity-hook-form/lib/esm/context/FormOptions.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import { PropsWithChildren } from 'react';
-export type FormOptions = {
-    showErrorText?: boolean;
-};
-export declare function ReactivityHookFormProvider({ children, showErrorText }: PropsWithChildren<FormOptions>): React.JSX.Element;
-export declare function useFormOptions(): FormOptions;
--- packages/reactivity-hook-form/lib/esm/index.js
+++
@@ -1,7 +0,0 @@
-export { default } from './components/Form';
-export * from './components/Form';
-export * from './components/FormItem';
-export * from './context/FormOptions';
-export * from './hooks/useCollapsibleExpandError';
-export * from './types/dependencies.type';
-export * from './types/validations.type';
